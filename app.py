fe75751f1f4c7c4fa428b88f44b8e1ff63d9cec8
zeni-design <zeniyah.pruitt@ascte.org>
2025-04-18 09:00:46 -0500

    backend accepts post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import json
 
 app = Flask(__name__)
@@ -12,6 +12,16 @@ def get_posts():
     with(open("data.json", "r")) as file:
         posts = json.load(file)
     return jsonify(posts)
+
+@app.route('/api/posts' , methods=['POST'])
+def add_posts():
+    new_post = request.get_json()
+    with(open("data.json", "r")) as file:
+        posts = json.load(file)
+        posts.insert(0, new_post) #add post at the top of the file
+    with(open("data.json", "r")) as file:
+        json.dump(posts, file, indent=4)
+    return jsonify({"status": "success"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
