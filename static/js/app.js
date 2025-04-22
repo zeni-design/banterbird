a3924341eda4bdd533cedbbb6cf4a6759ce445e7
zeni-design <zeniyah.pruitt@ascte.org>
2025-04-22 09:10:53 -0500

    posts show on the website

--- static/js/app.js
+++ static/js/app.js
@@ -1,24 +1,43 @@
 const username = "admin";
 
-function renderPost(post) {
+function renderPost(post, isNew = false) {
     const template = document.getElementById("post-template").content.cloneNode(true);
     template.querySelector(".username").innerText = post.username;
     template.querySelector(".message").innerText = post.message;
     document.getElementById("feed").appendChild(template);
+    if(isNew){
+        document.getElementById("feed").prepend(template);
+    }
+    else{
+        document.getElementById("feed").appendChild(template);
+    }
 }
 
-function submitPost() {
+async function submitPost() {
     const message = document.getElementById("postInput").value;
-    console.log("Would post:", message);
-    alert("Tweet submitted (not really yet)");
+    try {
+        const response = await fetch ("/api/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify({
+                username:username,
+                message:message,
+            }),
+        });
+        if (response.ok){
+            renderPost({username: username, message:message}, true)
+            document.getElementById("postInput").value = "";
+        }
+    } catch (error){
+        console.error("Error submitting post:", error);
+    }
 }
 
 window.onload = async () => {
     try {
-        const response = await fetch("/api/posts");
-        const posts = await response.json();
-        posts.forEach(post => renderPost(post));
-    } catch (error) {
-        console.error("Error fetching posts:", error);
+        const response = await fetch("api/posts");
+        const post 
     }
 };
